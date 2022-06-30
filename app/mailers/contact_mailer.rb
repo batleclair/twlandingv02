80f750b7ec38217ed851b5da65aed2706eaecd4a
Baptiste Clair <baptisteclair@gmail.com>
2022-06-30 21:32:22 +0200

    mailer including Paul

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,8 @@
 class ContactMailer < ApplicationMailer
   def new_contact_email
     @contact = params[:contact]
-    mail(to: ENV['ADMIN_EMAIL'], subject: "Nouveau formulaire de contact reçu ! 🥳")
+    mail(to: [ENV['ADMIN_EMAIL'], ENV['ADMIN2_EMAIL']], subject: "Nouveau formulaire de contact reçu ! 🥳")
   end
+
+
 end
