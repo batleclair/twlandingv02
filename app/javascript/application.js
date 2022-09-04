3201d84745130eca2ceff5628116aa16b360e437
Baptiste Clair <baptisteclair@gmail.com>
2022-09-04 10:16:10 +0200

    test

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,3 +7,15 @@ import "@rails/actiontext"
 import { initFlatpickr } from "./plugins/flatpickr";
 
 initFlatpickr();
+
+import "@rails/actiontext";
+import { AttachmentUpload } from "@rails/actiontext/app/javascript/actiontext/attachment_upload"
+
+addEventListener("trix-attachment-add", event => {
+  const { attachment, target } = event
+
+  if (attachment.file) {
+    const upload = new AttachmentUpload(attachment, target)
+    upload.start()
+  }
+})
