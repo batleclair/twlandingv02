4ead47bc0b191273aa8889cc8a89d42560e1218c
Baptiste Clair <baptisteclair@gmail.com>
2022-12-21 17:26:26 +0100

    adding contact blacklist

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,14 @@
 class Contact < ApplicationRecord
-  CONTACT_TYPES = ["Association", "Entreprise", "Salarié"]
+  CONTACT_BLACKLIST = [
+    "ericjonesmyemail@gmail.com"
+  ]
+
   validates :first_name, presence: { message: "Veuillez renseigner votre prénom" }
   validates :last_name, presence: { message: "Veuillez renseigner votre nom" }
   validates :organization, presence: { message: "Merci d'indiquer votre entreprise ou association" }
   validates :contact_type, presence: { message: "Veuillez sélectionner parmi les options" }
   validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Veuillez renseigner une adresse valide" }
+  validates :email, exclusion: { in: CONTACT_BLACKLIST, message: "🛑 vous avez été identifié·e comme spammer" }
+
+  CONTACT_TYPES = ["Association", "Entreprise", "Salarié"]
 end
