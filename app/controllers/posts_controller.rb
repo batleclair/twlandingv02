8a8a12ff38c4414a92dabb1590d3925ce1cb57cc
Baptiste Clair <baptisteclair@gmail.com>
2023-01-06 12:04:56 +0100

    improving blog

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @posts = policy_scope(Post)
+    @posts = policy_scope(Post).order(created_at: :desc)
     @contact = Contact.new
   end
 
