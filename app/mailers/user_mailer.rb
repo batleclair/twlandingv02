4bb070fc0d68764df1cab11e7f87cd7a47c97e4c
Baptiste Clair <baptisteclair@gmail.com>
2023-05-14 15:36:51 +0200

    fixing logo inline mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def new_user_email(user)
     @user = user
+    attachments.inline["logo_dark.png"] = File.read("#{Rails.root}/app/assets/images/logo_dark.png")
     mail(to: @user.email, subject: "Bienvenue chez Demain ! 🙌")
   end
 end
