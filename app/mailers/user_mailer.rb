a14010f0b1e9a8fe2ff48a68b07663f4581d090f
Baptiste Clair <baptisteclair@gmail.com>
2023-05-19 19:06:33 +0200

    improving mail format cross clients

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,8 @@ class UserMailer < ApplicationMailer
   def new_user_email(user)
     @user = user
     attachments.inline["logo_dark.png"] = File.read("#{Rails.root}/app/assets/images/logo_dark.png")
+    attachments.inline["profile_btn.png"] = File.read("#{Rails.root}/app/assets/images/profile_btn.png")
+    attachments.inline["missions_btn.png"] = File.read("#{Rails.root}/app/assets/images/missions_btn.png")
     mail(to: @user.email, subject: "Bienvenue chez Demain ! 🙌")
   end
 
@@ -9,7 +11,7 @@ class UserMailer < ApplicationMailer
     @user = user
     @token = token
     attachments.inline["logo_dark.png"] = File.read("#{Rails.root}/app/assets/images/logo_dark.png")
+    attachments.inline["complete_btn.png"] = File.read("#{Rails.root}/app/assets/images/complete_btn.png")
     mail(to: @user.email, subject: "Votre invitation Demain ! 🙌")
   end
-
 end
