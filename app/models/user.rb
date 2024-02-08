28a2fbbc9ba17fc9ac390b54d94aca47d6f49d29
Baptiste Clair <baptisteclair@gmail.com>
2024-02-08 09:27:40 +0100

    no confirmaiton email when oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,7 +44,8 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
-      user.skip_confirmation!
+      user.confirmed_at = DateTime.now # inline skip_confirmation!
+      resource.skip_confirmation_notification!
       # user.attach_custom_id if user.whitelisted?
       # raise
     end
