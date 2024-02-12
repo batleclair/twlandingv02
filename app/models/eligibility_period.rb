dd25a60a64bd2226d4feb1ca0fceb0107a97489c
Baptiste Clair <baptisteclair@gmail.com>
2024-02-12 18:45:19 +0100

    destroy period dependant nullify for employee applications

--- app/models/eligibility_period.rb
+++ app/models/eligibility_period.rb
@@ -1,8 +1,9 @@
 class EligibilityPeriod < ApplicationRecord
   belongs_to :company
   validates :title, presence: {message: "Titre requis"}
+  has_many :employee_applications, dependent: :nullify
 
   def title_and_dates
-    "#{title} | #{start_date.strftime("%d/%m/%Y")} -> #{end_date.strftime("%d/%m/%Y")}"
+    "#{title} | #{start_date&.strftime("%d/%m/%Y")} -> #{end_date&.strftime("%d/%m/%Y")}"
   end
 end
